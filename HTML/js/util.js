734df46c67d49fe3c4027c34afe2143b870b0487
ericksantana <erickesantana@gmail.com>
2014-07-26 20:01:48 -0300

    ajuste de menu

---
+++ HTML/js/util.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+//Adiciona classe "active" no menu clicado.
+	$('.lista li').click(function(){
+		$('.lista li').removeClass('active');
+		$(this).addClass('active');
+	});
+});
