32e24b15585605f74126d8e8192497a2adf38bf0
ericksantana <erickesantana@gmail.com>
2014-07-29 01:09:16 -0300

    ancora de menu

--- HTML/js/util.js
+++ HTML/js/util.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
 		$(this).addClass('active');
 	});
 
+	//Cria animação de ancora.
+	$.fn.ancora = function(){
+	        $('html,body').animate({scrollTop:$(this).offset().top});
+	}
+	//quem tiver # no href, tem animação.
+	$(function(){
+	        $('a[href^=#]').click(function(){
+	                $($(this).attr('href')).ancora();
+	                return false;
+	        })
+	});
+
 });
